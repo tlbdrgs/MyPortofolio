c6c87c760498ba347ceea0acb4a6d01050acef81
Dragos <talaba.dragos@gmail.com>
2024-06-11 01:14:21 +0300

    code formatting

--- portfolio/components/header.tsx
+++ portfolio/components/header.tsx
@@ -13,14 +13,16 @@ export default function Header() {
          initial={{ y:-100, x: "-50%", opacity: 0}}
          animate={{ y:0, x: "-50%", opacity: 1}}
          >
-
          </motion.div>
          <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
                 <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem]
                  font-medium text-slate-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
                     {
                         links.map(link => (
-                            <motion.li className="h-3/4 flex items-center justify-center" initial={{y:-100, opacity:0}} animate={{y:0, opacity:1}} key={link.hash}>
+                            <motion.li className="h-3/4 flex items-center justify-center"
+                            initial={{y:-100, opacity:0}}
+                            animate={{y:0, opacity:1}}
+                            key={link.hash}>
                                 <Link className="flex w-full items-center justify-center px-3 py-3 hover:text-slate-950 transition" href={link.hash}>
                                 { link.name }
                                 </Link>
